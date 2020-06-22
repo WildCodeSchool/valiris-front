7e9b527121617c0cf360a682e073f23b94eac4b9
K0Si-003 <hugo.pioda@gmail.com>
2020-06-22 14:19:46 +0200

    implement map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import '../styles/Map.css';
 
 const MapComponent = () => {
+  const position = [45.8322560, 4.7753658];
   return (
-    <Map center={[45.4, -75.7]} zoom={12}>
+    <Map center={position} zoom={14}>
       <TileLayer
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <Marker
+        position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </Map>
   );
 };
