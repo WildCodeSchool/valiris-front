822a638b75d1dc1c047d40f2efa737e97925db47
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-06-22 12:14:25 +0200

    installs react full calendar

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Calendar from './Calendar';
+import '../styles/booking.css';
 
 const Booking = () => {
   const { t } = useTranslation();
@@ -9,6 +11,7 @@ const Booking = () => {
       <p>
       Tarif toutes charges comprises (Eau, Electricité, chauffage, accès internet wifi, assurance occupant).                                                                                                                            À la semaine à partir de 480 €.                                                                                                                                                                                                                                                                        Au mois, à partir de 790 €.                                                                                                                                                                                                                                                         Acompte à la réservation de 1/3 par virement, le solde à l'arrivée par CB.                                                                                                                                                              Caution de garantie de 500 € par CB à l'arrivée, non prélevée.                                                                                                                                                                           Remboursement de l'acompte si annulation au moins 14 jours avant la date prévue d'arrivée.                                                                                                     Remboursement à 50% de l'acompte si annulation au moins 4 jours avant la date prévus d'arrivée.                                                                                                                          Pas de remboursement si annulation à moins de 4 jours de la date prévue d'arrivée.
       </p>
+      <Calendar />
     </div>
   );
 };
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
-export const Calendar = () => {
+const Calendar = () => {
   return (
-    <div />
+    <div className='booking-calendar' style={{width: '70%'}}>
+      <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+        events={[
+            {
+              id: '1',
+              title: 'Glycines',
+              start: '2020-06-22',
+              end: '2020-06-28',
+              color: 'red',
+            },
+            {
+              id: '2',
+              title: 'Chasselas',
+              start: '2020-06-22',
+              end: '2020-06-28',
+              color: 'blue',
+            },
+            {
+              id: '3',
+              title: 'Pêches de Vigne',
+              start: '2020-06-22',
+              end: '2020-06-28',
+              color: 'green',
+            },
+            {
+              id: '4',
+              title: 'Bigarreaux Burlat',
+              start: '2020-06-22',
+              end: '2020-06-28',
+              color: 'orange',
+            },
+        ]}
+      />
+    </div>
   );
 };
+
+export default Calendar;
