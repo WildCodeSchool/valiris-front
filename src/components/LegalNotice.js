c5b69b2dfa2b53b2b160afce9d1e57eed4de1a98
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-07-07 17:59:47 +0200

    updates legal notice

--- src/components/LegalNotice.js
+++ src/components/LegalNotice.js
@@ -2,10 +2,45 @@ import React from 'react';
 
 const LegalNotice = () => {
   return (
-    <div>
-      <h1>Mentions Légales</h1>
-      <p />
-    </div>
+  <div>
+      <p style={{textAlign: 'center'}}>
+        Si vous utilisez nos Mentions Légales, vous vous engagez à laisser les liens <strong>Crédit sans en modifier ni les liens ni les ancres.</strong>
+        <br />
+        <span style={{color: 'rgb(255, 0, 0)'}}>Par avance merci pour votre compréhension et le respect du travail effectué par toute l'équipe</span>
+      </p>
+
+      <h2>MENTIONS LEGALES :</h2>
+
+      <p style={{textAlign: 'justify'}}><br />
+        Conformément aux dispositions des articles 6-III et 19 de la Loi n° 2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., nous portons à la connaissance des utilisateurs et visiteurs du site : <a href='http://www.exemple.com' target='_blank'>www.exemple.com</a> les informations suivantes :
+      </p>
+
+      <p style={{textAlign: 'justify'}}><strong>ÉDITEUR</strong></p>
+
+      <p style={{textAlign: 'justify'}}>Le site est la propriété exclusive de <strong>SCI Gervais Limonest</strong>, qui l'édite.</p>
+
+      <p>
+        <strong>SCI </strong>au capital de 850 000€ Tél  : 0000000000
+      </p>
+
+      <p style={{textAlign: 'justify'}}><strong>470 Route de Saint Didier 69760 Limonest</strong><br />
+        Immatriculée au Registre du Commerce et des Sociétés de  <strong>Lyon sous le numéro 75337080800010</strong>
+      </p>
+
+      <p style={{textAlign: 'justify'}}>Numéro TVA intracommunautaire : <strong>00</strong><br />
+        Adresse de courrier électronique : <strong>stephane@valiris.fr</strong> 
+        <br />
+        <br />
+        Directeur de la  publication : <strong>Stephane Gervais</strong><br />
+        Contactez le responsable de la publication : <strong>stephane@valiris.fr</strong>
+      </p>
+
+      <p style={{textAlign: 'justify'}}><strong>HÉBERGEMENT</strong></p>
+
+      <p style={{textAlign: 'justify'}}>Le site est hébergé par <strong>No info No info 69000 Lyon</strong><br />
+      </p>
+  </div>
+      
   );
 };
 
